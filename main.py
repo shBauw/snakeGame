dc94c6ace5f8c039c0010146fa1742850cd824f4
shBauw <ali.kachwalla@gmail.com>
2022-01-28 21:51:19 +1100

    2 main bugs: seems to be a limit on length, graphics draw weirdly, also I don't know if the overlap check functions correctly

--- main.py
+++ main.py
@@ -17,10 +17,12 @@ size = width, height = 500, 500
 screen = pg.display.set_mode(size)
 
 # Set initial position
-xInit = 250
-yInit = 250
-square = 20
-clock = pg.time.Clock()
+xPos = 250
+yPos = 250
+square = 15
+xSnake = [xPos]
+ySnake = [yPos]
+snakeLength = 1
 
 # Movement
 xChange = 0
@@ -44,14 +46,18 @@ while True:
         randomy = round(random.randrange(square, height - square) / square) * square
 
         # Check to make sure apple not spawned on snake
-        if round(xInit / square) != round(randomx / square) and round(yInit / square) != round(randomy / square):
+        if round(xPos / square) != round(randomx / square) and round(yPos / square) != round(randomy / square):
             apple = True
     
     # Check if apples are eaten
-    if round(xInit / square) == round(randomx / square) and round(yInit / square) == round(randomy / square):
+    if round(xPos / square) == round(randomx / square) and round(yPos / square) == round(randomy / square):
         apple = False
         score += 1
         speed = 15 + score
+
+        xSnake.append(xPos - xChange)
+        ySnake.append(yPos - yChange)
+        snakeLength += 1
 
     # Event loop
     for event in pg.event.get():
@@ -63,15 +69,15 @@ while True:
         if event.type == pg.KEYDOWN:
             if event.key == pg.K_UP:
                 xChange = 0
-                yChange = -10
+                yChange = -15
             elif event.key == pg.K_DOWN:
                 xChange = 0
-                yChange = 10
+                yChange = 15
             elif event.key == pg.K_RIGHT:
-                xChange = 10
+                xChange = 15
                 yChange = 0
             elif event.key == pg.K_LEFT:
-                xChange = -10
+                xChange = -15
                 yChange = 0
             # Create pause menu
             elif event.key == pg.K_ESCAPE:
@@ -91,10 +97,8 @@ while True:
                                 pauseGame = False
         
         # Set border
-        if xInit >= width or xInit < 0 or yInit >= height or yInit < 0:
+        if xPos >= width or xPos < 0 or yPos >= height or yPos < 0:
             gameOver = True
-        
-        # Create condition for snake overlapping itself
 
         # Ending condition
         while gameOver == True:
@@ -111,17 +115,39 @@ while True:
                         sys.exit()
                     if event.key == pg.K_r:
                         score = 0
-                        xInit = 250
-                        yInit = 250
+                        xPos = 250
+                        yPos = 250
+                        xSnake = [xPos]
+                        ySnake = [yPos]
+                        snakeLength = 1
+                        xChange = 0
+                        yChange = 0
+                        speed = 15
                         gameOver = False
     
-    # Movement and refresh screen
-    xInit += xChange
-    yInit += yChange
+    # Movement
+    xPos += xChange
+    yPos += yChange
+    xSnake.append(xPos)
+    ySnake.append(yPos)
+
+    # Remove end of snake
+    while len(xSnake) > snakeLength:
+        del xSnake[0] 
+        del ySnake[0]
+
+    # Check for overlapping
+    for x in range(snakeLength - 1):
+        if (xSnake[0] == xSnake[x + 1] and ySnake[0] == ySnake[x + 1]):
+            gameOver = True
+
+    # Refresh screen
     screen.fill(black)
-    pg.draw.rect(screen, green, [xInit, yInit, square, square])
+    for x in range(len(xSnake)):
+        for y in range(len(ySnake)):
+            pg.draw.rect(screen, green, [xSnake[x], ySnake[y], square, square])
     pg.draw.rect(screen, red, [randomx, randomy, square, square])
     screen.blit(pg.font.SysFont("None", 30).render("Score: " + str(score), True, green), [width / 10, height / 10])
     pg.display.update()
 
-    clock.tick(speed)
+    pg.time.Clock().tick(speed)
