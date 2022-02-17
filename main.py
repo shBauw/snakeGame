16caf88b77937a36a2ea675fc296747573ce9f0c
shBauw <ali.kachwalla@gmail.com>
2022-02-18 10:00:27 +1100

    Cleaned code

--- main.py
+++ main.py
@@ -16,140 +16,134 @@ blue = 0, 0, 255
 size = width, height = 400, 400
 screen = pg.display.set_mode(size)
 
-# Set initial position
-xPos = 200
-yPos = 200
-square = 20
-xSnake = [xPos]
-ySnake = [yPos]
-snakeLength = 1
-
-# Movement
-xChange = 0
-yChange = 0
-speed = 10
-
-# Set scopes for conditions
-apple = False
-gameOver = False
-randomx = 0
-randomy = 0
-score = 0
-pauseGame = False
-
-# Loop for game
 while True:
+    # Setting inital positions
+    xPos = 200
+    yPos = 200
+    square = 20
+    xSnake = [xPos]
+    ySnake = [yPos]
+    snakeLength = 1
 
-    # Spawn new apple
-    if apple == False or gameOver == True:
-        randomx = round(random.randrange(square, width - square) / square) * square
-        randomy = round(random.randrange(square, height - square) / square) * square
+    # Setting initial conditions
+    apple = False
+    gameOver = False
+    randomx = 0
+    randomy = 0
+    score = 0
+    pauseGame = False
+    gameEnd = False
 
-        # Check to make sure apple not spawned on snake
-        if round(xPos / square) != round(randomx / square) and round(yPos / square) != round(randomy / square):
-            apple = True
-    
-    # Check if apples are eaten
-    if xPos == randomx and yPos == randomy:
-        apple = False
-        score += 1
-        speed += 1
-
-        xSnake.append(xPos - xChange)
-        ySnake.append(yPos - yChange)
-        snakeLength += 1
-
-    # Set border
-    if xPos >= width or xPos < 0 or yPos >= height or yPos < 0:
-        gameOver = True
-
-    # Ending condition
-    if gameOver == True:
-        screen.fill(black)
-        screen.blit(pg.font.SysFont(None, 30).render("Q = Quit, R = Retry", True, green), [width / 4, height / 4])
-        pg.display.update()
-
-    # Event loop
-    if gameOver == False:
-        for event in pg.event.get():
-            # Quit
-            if event.type == pg.QUIT:
-                sys.exit()
-            
-            # Change directions
-            if event.type == pg.KEYDOWN:
-                if event.key == pg.K_UP:
-                    xChange = 0
-                    yChange = -20
-                elif event.key == pg.K_DOWN:
-                    xChange = 0
-                    yChange = 20
-                elif event.key == pg.K_RIGHT:
-                    xChange = 20
-                    yChange = 0
-                elif event.key == pg.K_LEFT:
-                    xChange = -20
-                    yChange = 0
-                # Create pause menu
-                elif event.key == pg.K_ESCAPE:
-                    pauseGame = True
-                    while pauseGame == True:
-                        screen.fill(black)
-                        screen.blit(pg.font.SysFont("None", 30).render("Score: " + str(score), True, green), [width / 10, height / 10])
-                        screen.blit(pg.font.SysFont(None, 30).render("Q = Quit, C = Continue", True, green), [width / 4, height / 4])
-                        pg.display.update()
-                        for event in pg.event.get():
-                            if event.type == pg.QUIT:
-                                sys.exit()
-                            elif event.type == pg.KEYDOWN:
-                                if event.key == pg.K_q:
-                                    sys.exit()
-                                elif event.key == pg.K_c:
-                                    pauseGame = False
-        
-        
-    while gameOver == True:
-        # Quit or restart
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                sys.exit()
-            elif event.type == pg.KEYDOWN:
-                if event.key == pg.K_q:
-                    sys.exit()
-                if event.key == pg.K_r:
-                    score = 0
-                    xPos = 250
-                    yPos = 250
-                    xSnake = [xPos]
-                    ySnake = [yPos]
-                    snakeLength = 1
-                    xChange = 0
-                    yChange = 0
-                    speed = 10
-                    gameOver = False
-    
     # Movement
-    xPos += xChange
-    yPos += yChange
-    xSnake.append(xPos)
-    ySnake.append(yPos)
+    xChange = 0
+    yChange = 0
+    speed = 10
 
-    # Remove end of snake
-    while len(xSnake) > snakeLength:
-        del xSnake[0] 
-        del ySnake[0]
+    # Loop for game
+    while gameEnd == False:
 
-    # Check for overlapping
-    for i in range(snakeLength - 1):
-        if (xSnake[snakeLength - 1] == xSnake[i] and ySnake[snakeLength - 1] == ySnake[i]):
+        # Spawn new apple
+        if apple == False or gameOver == True:
+            randomx = round(random.randrange(square, width - square) / square) * square
+            randomy = round(random.randrange(square, height - square) / square) * square
+
+            # Check to make sure apple not spawned on snake
+            if round(xPos / square) != round(randomx / square) and round(yPos / square) != round(randomy / square):
+                apple = True
+        
+        # Check if apples are eaten
+        if xPos == randomx and yPos == randomy:
+            apple = False
+            score += 1
+            speed += 1
+
+            xSnake.append(xPos - xChange)
+            ySnake.append(yPos - yChange)
+            snakeLength += 1
+
+        # Set border
+        if xPos >= width or xPos < 0 or yPos >= height or yPos < 0:
             gameOver = True
 
-    # Refresh screen
-    screen.fill(black)
-    for i in range(snakeLength):
-        pg.draw.rect(screen, green, [xSnake[i], ySnake[i], square, square])
-    pg.draw.rect(screen, red, [randomx, randomy, square, square])
-    screen.blit(pg.font.SysFont("None", 30).render("Score: " + str(score), True, green), [width / 10, height / 10])
-    pg.display.update()
+        # Ending condition
+        if gameOver == True:
+            screen.fill(black)
+            screen.blit(pg.font.SysFont(None, 30).render("Q = Quit, R = Retry", True, green), [width / 4, height / 4])
+            pg.display.update()
 
-    pg.time.Clock().tick(speed)
+        # Event loop
+        if gameOver == False:
+            for event in pg.event.get():
+                # Quit
+                if event.type == pg.QUIT:
+                    sys.exit()
+                
+                # Change directions
+                if event.type == pg.KEYDOWN:
+                    if event.key == pg.K_UP:
+                        xChange = 0
+                        yChange = -20
+                    elif event.key == pg.K_DOWN:
+                        xChange = 0
+                        yChange = 20
+                    elif event.key == pg.K_RIGHT:
+                        xChange = 20
+                        yChange = 0
+                    elif event.key == pg.K_LEFT:
+                        xChange = -20
+                        yChange = 0
+                    # Create pause menu
+                    elif event.key == pg.K_ESCAPE:
+                        pauseGame = True
+                        while pauseGame == True:
+                            screen.fill(black)
+                            screen.blit(pg.font.SysFont("None", 30).render("Score: " + str(score), True, green), [width / 10, height / 10])
+                            screen.blit(pg.font.SysFont(None, 30).render("Q = Quit, C = Continue", True, green), [width / 4, height / 4])
+                            pg.display.update()
+                            for event in pg.event.get():
+                                if event.type == pg.QUIT:
+                                    sys.exit()
+                                elif event.type == pg.KEYDOWN:
+                                    if event.key == pg.K_q:
+                                        sys.exit()
+                                    elif event.key == pg.K_c:
+                                        pauseGame = False
+            
+            
+        while gameOver == True:
+            # Quit or restart
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    sys.exit()
+                elif event.type == pg.KEYDOWN:
+                    if event.key == pg.K_q:
+                        sys.exit()
+                    if event.key == pg.K_r:
+                        gameOver = False
+                        gameEnd = True
+        
+        # Movement
+        xPos += xChange
+        yPos += yChange
+        xSnake.append(xPos)
+        ySnake.append(yPos)
+
+        # Remove end of snake
+        while len(xSnake) > snakeLength:
+            del xSnake[0] 
+            del ySnake[0]
+
+        # Check for overlapping
+        for i in range(snakeLength - 1):
+            if (xSnake[snakeLength - 1] == xSnake[i] and ySnake[snakeLength - 1] == ySnake[i]):
+                gameOver = True
+
+        # Refresh screen
+        screen.fill(black)
+        for i in range(snakeLength):
+            pg.draw.rect(screen, green, [xSnake[i], ySnake[i], square, square])
+        pg.draw.rect(screen, red, [randomx, randomy, square, square])
+        screen.blit(pg.font.SysFont("None", 30).render("Score: " + str(score), True, green), [width / 10, height / 10])
+        pg.display.update()
+
+        pg.time.Clock().tick(speed)
