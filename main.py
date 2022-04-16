8cbb7bbf25b78a3135437c7c0bd1b1ffa0a8c82f
shBauw <ali.kachwalla@gmail.com>
2022-04-16 17:21:04 +1000

    OOP

--- main.py
+++ main.py
@@ -1,151 +1,180 @@
 # Import needed libraries
 import random
+from re import X
 import pygame as pg
 import sys
 
-# Initiate pyGame
-pg.init()
+class pod(object):
+    rows = 20
+    width = 500
+    def __init__(self, start, dirx=1, diry=0, colour=(0,255,0)):
+        self.pos = start
+        self.dirx = 1
+        self.diry = 0
+        self.colour = colour
 
-# Set default colours
-black = 0, 0, 0
-red = 255, 0, 0
-green = 0, 255, 0
-blue = 0, 0, 255
+    def move(self, dirx, diry):
+        self.dirx = dirx
+        self.diry = diry
+        self.pos = (self.pos[0] + dirx, self.pos[1] + diry)
 
-# Set window size
-size = width, height = 400, 400
-screen = pg.display.set_mode(size)
+    def draw(self, surface):
+        display = self.width // self.rows
+        x = self.pos[0]
+        y = self.pos[1]
 
-# Define message
-def message(msg, w, h):
-    screen.blit(pg.font.SysFont(None, 30).render(msg, True, green), [width / w, height / h])
+        pg.draw.rect(surface, self.colour, (x * display + 1, y * display + 1, display - 2, display - 2))
+
+class snake(object):
+    body = []
+    turns = {}
+    def __init__(self, colour, pos):
+        self.colour = colour
+        self.head = pod(pos)
+        self.body.append(self.head)
+        self.dirx = 0
+        self.diry = 1
+
+    def move(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+            elif event.type == pg.KEYDOWN:
+                if event.key == pg.K_ESCAPE:
+                    pg.quit()
+                elif event.key == pg.K_UP:
+                    self.dirx = 0
+                    self.diry = -1
+                    self.turns[self.head.pos[:]] = [self.dirx, self.diry]
+                elif event.key == pg.K_DOWN:
+                    self.dirx = 0
+                    self.diry = 1
+                    self.turns[self.head.pos[:]] = [self.dirx, self.diry]
+                elif event.key == pg.K_RIGHT:
+                    self.dirx = 1
+                    self.diry = 0
+                    self.turns[self.head.pos[:]] = [self.dirx, self.diry]
+                elif event.key == pg.K_LEFT:
+                    self.dirx = -1
+                    self.diry = 0
+                    self.turns[self.head.pos[:]] = [self.dirx, self.diry]         
+        
+        for count, index in enumerate(self.body):
+            p = index.pos[:]
+            if p in self.turns:
+                turn = self.turns[p]
+                index.move(turn[0], turn[1])
+                if count == len(self.body) - 1:
+                    self.turns.pop(p)
+            else:
+                if index.dirx == -1 and index.pos[0] <= 0: 
+                    print('Score: ', len(s.body))
+                    s.reset((10,10))
+                elif index.dirx == 1 and index.pos[0] >= index.rows-1: 
+                    print('Score: ', len(s.body))
+                    s.reset((10,10))
+                elif index.diry == 1 and index.pos[1] >= index.rows-1: 
+                    print('Score: ', len(s.body))
+                    s.reset((10,10))
+                elif index.diry == -1 and index.pos[1] <= 0: 
+                    print('Score: ', len(s.body))
+                    s.reset((10,10))
+                else: 
+                    index.move(index.dirx,index.diry)
+
+    def reset(self, pos):
+        self.head = pod(pos)
+        self.body = [self.head]
+        self.turns = {}
+        self.dirx = 0
+        self.diry = 1
+
+
+    def addCube(self):
+        tail = self.body[-1]
+        dx, dy = tail.dirx, tail.diry
+
+        if dx == 1 and dy == 0:
+            self.body.append(pod((tail.pos[0]-1,tail.pos[1])))
+        elif dx == -1 and dy == 0:
+            self.body.append(pod((tail.pos[0]+1,tail.pos[1])))
+        elif dx == 0 and dy == 1:
+            self.body.append(pod((tail.pos[0],tail.pos[1]-1)))
+        elif dx == 0 and dy == -1:
+            self.body.append(pod((tail.pos[0],tail.pos[1]+1)))
+
+        self.body[-1].dirx = dx
+        self.body[-1].diry = dy
+        
+
+    def draw(self, surface):
+        for i, c in enumerate(self.body):
+            c.draw(surface)
+
+
+def drawGrid(width, rows, surface):
+    sizeBtwn = width // rows
+
+    x = 0
+    y = 0
+    for l in range(rows):
+        x = x + sizeBtwn
+        y = y + sizeBtwn
+
+        pg.draw.line(surface, (255,255,255), (x,0),(x,width))
+        pg.draw.line(surface, (255,255,255), (0,y),(width,y))
+        
+
+def redrawWindow(surface):
+    global rows, width, s, snack
+    surface.fill((0,0,0))
+    s.draw(surface)
+    snack.draw(surface)
+    drawGrid(width, rows, surface)
     pg.display.update()
 
-while True:
-    # Setting inital positions
-    xPos = 200
-    yPos = 200
-    square = 20
-    xSnake = [xPos]
-    ySnake = [yPos]
-    snakeLength = 1
 
-    # Setting initial conditions
-    apple = False
-    gameOver = False
-    randomx = 0
-    randomy = 0
-    score = 0
-    pauseGame = False
-    gameEnd = False
+def randomSnack(rows, item):
 
-    # Movement
-    xChange = 0
-    yChange = 0
-    speed = 10
+    positions = item.body
 
-    # Loop for game
-    while gameEnd == False:
-
-        # Spawn new apple
-        if apple == False or gameOver == True:
-            randomx = round(random.randrange(square, width - square) / square) * square
-            randomy = round(random.randrange(square, height - square) / square) * square
-
-            # Check to make sure apple not spawned on snake
-            if round(xPos / square) != round(randomx / square) and round(yPos / square) != round(randomy / square):
-                apple = True
+    while True:
+        x = random.randrange(rows)
+        y = random.randrange(rows)
+        if len(list(filter(lambda z:z.pos == (x,y), positions))) > 0:
+            continue
+        else:
+            break
         
-        # Check if apples are eaten
-        if xPos == randomx and yPos == randomy:
-            apple = False
-            score += 1
-            speed += 1
+    return (x,y)
 
-            xSnake.append(xPos - xChange)
-            ySnake.append(yPos - yChange)
-            snakeLength += 1
 
-        # Set border
-        if xPos >= width or xPos < 0 or yPos >= height or yPos < 0:
-            gameOver = True
+def main():
+    global width, rows, s, snack
+    width = 500
+    rows = 20
+    win = pg.display.set_mode((width, width))
+    s = snake((255,0,0), (10,10))
+    snack = pod(randomSnack(rows, s), colour=(255,0,0))
+    flag = True
 
-        # Ending condition
-        if gameOver == True:
-            screen.fill(black)
-            message("Q = Quit, R = Retry", 4, 4)
+    clock = pg.time.Clock()
+    
+    while flag:
+        pg.time.delay(50)
+        clock.tick(10)
+        s.move()
+        if s.body[0].pos == snack.pos:
+            s.addCube()
+            snack = pod(randomSnack(rows, s), colour=(255,0,0))
 
-        # Event loop
-        if gameOver == False:
-            for event in pg.event.get():
-                # Quit
-                if event.type == pg.QUIT:
-                    sys.exit()
-                
-                # Change directions
-                if event.type == pg.KEYDOWN:
-                    if event.key == pg.K_UP:
-                        xChange = 0
-                        yChange = -20
-                    elif event.key == pg.K_DOWN:
-                        xChange = 0
-                        yChange = 20
-                    elif event.key == pg.K_RIGHT:
-                        xChange = 20
-                        yChange = 0
-                    elif event.key == pg.K_LEFT:
-                        xChange = -20
-                        yChange = 0
-                    # Create pause menu
-                    elif event.key == pg.K_ESCAPE:
-                        pauseGame = True
-                        while pauseGame == True:
-                            screen.fill(black)
-                            message("Score: " + str(score), 10, 10)
-                            message("Q = Quit, C = Continue", 4, 4)
-                            for event in pg.event.get():
-                                if event.type == pg.QUIT:
-                                    sys.exit()
-                                elif event.type == pg.KEYDOWN:
-                                    if event.key == pg.K_q:
-                                        sys.exit()
-                                    elif event.key == pg.K_c:
-                                        pauseGame = False
+        for x in range(len(s.body)):
+            if s.body[x].pos in list(map(lambda z:z.pos,s.body[x+1:])):
+                print('Score: ', len(s.body))
+                s.reset((10,10))
+                break
+
             
-            
-        while gameOver == True:
-            # Quit or restart
-            for event in pg.event.get():
-                if event.type == pg.QUIT:
-                    sys.exit()
-                elif event.type == pg.KEYDOWN:
-                    if event.key == pg.K_q:
-                        sys.exit()
-                    if event.key == pg.K_r:
-                        gameOver = False
-                        gameEnd = True
-        
-        # Movement
-        xPos += xChange
-        yPos += yChange
-        xSnake.append(xPos)
-        ySnake.append(yPos)
+        redrawWindow(win)
 
-        # Remove end of snake
-        while len(xSnake) > snakeLength:
-            del xSnake[0] 
-            del ySnake[0]
-
-        # Check for overlapping
-        for i in range(snakeLength - 1):
-            if (xSnake[snakeLength - 1] == xSnake[i] and ySnake[snakeLength - 1] == ySnake[i]):
-                gameOver = True
-
-        # Refresh screen
-        screen.fill(black)
-        for i in range(snakeLength):
-            pg.draw.rect(screen, green, [xSnake[i], ySnake[i], square, square])
-        pg.draw.rect(screen, red, [randomx, randomy, square, square])
-        message("Score: " + str(score), 10, 10)
-
-        pg.time.Clock().tick(speed)
+main()
